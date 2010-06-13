003e6ec7c54a0aba7ade3ac0b0416881e6c6ef52
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2010-06-13 22:17:23 +0200

    Added logger and exception handling

---
+++ src/Exception.cpp
@@ -0,0 +1,27 @@
+/*
+ * QPhotoView support classes
+ *
+ * License: GPL V2. See file COPYING for details.
+ *
+ * Author:  Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
+ */
+
+#include "Exception.h"
+
+
+void Exception::setSrcLocation( const QString &srcFile,
+                                int            srcLine,
+                                const QString &srcFunction ) const
+{
+    // This is why those member variables are 'mutable':
+    // We need to be able to set the source location from RETHROW even after
+    // the exception was caught as const reference.
+    //
+    // This is not 100% elegant, but it keeps in line with usual conventions -
+    // conventions like "catch exception objects as const reference".
+
+    _srcFile     = srcFile;
+    _srcLine     = srcLine;
+    _srcFunction = srcFunction;
+}
+
